a6ba65cdf13223a22a975a2b35ee4025147babab
scx567888 <scx567888@outlook.com>
2022-02-11 14:19:27 +0800

    dev

--- test/src/router.js
+++ test/src/router.js
@@ -7,63 +7,40 @@ const ScxRouter = createRouter({
             path: '/scx-drag-test',
             name: 'scx-drag-test',
             component: () => import('./views/scx-drag-test.vue'),
-            meta: {
-                icon: 'filled-message',
-                perms: []
-            },
         },
         {
             path: '/scx-contextmenu-test',
             name: 'scx-contextmenu-test',
             component: () => import('./views/scx-contextmenu-test.vue'),
-            meta: {
-                icon: 'filled-message',
-                perms: []
-            },
         },
         {
             path: '/scx-eventbus-test',
             name: 'scx-eventbus-test',
             component: () => import('./views/scx-eventbus-test.vue'),
-            meta: {
-                icon: 'filled-message',
-                perms: []
-            },
         },
         {
             path: '/scx-icon-test',
             name: 'scx-icon-test',
             component: () => import('./views/scx-icon-test.vue'),
-            meta: {
-                icon: 'filled-message',
-                perms: []
-            },
         },
         {
             path: '/scx-xlsx-test',
             name: 'scx-xlsx-test',
             component: () => import('./views/scx-xlsx-test.vue'),
-            meta: {
-                icon: 'filled-message',
-                perms: []
-            },
         },
         {
             path: '/scx-crud-test',
             name: 'scx-crud-test',
             component: () => import('./views/scx-crud-test.vue'),
-            meta: {
-                icon: 'filled-message',
-                perms: []
-            },
         }, {
             path: '/scx-group-test',
             name: 'scx-group-test',
             component: () => import('./views/scx-group-test.vue'),
-            meta: {
-                icon: 'filled-message',
-                perms: []
-            },
+        },
+        {
+            path: '/listToTree-test',
+            name: 'listToTree-test',
+            component: () => import('./views/listToTree-test.vue'),
         },
     ]
 });
--- vanilla-listToTree.js
+++ vanilla-listToTree.js
@@ -1,21 +1,41 @@
 /**
  * 将含有 id parentID 的 list 列表转换为树形结构
- * 这里将 parentID === 0 的设置为 第一层
- * list 格式要求 [{parentID:0, id:10}, {parentID:10, id:100}]
+ * 这里将 parentID 为 null 或 undefined 的设置为 第一层
+ * list 格式要求 [{parentID:null, id:10}, {parentID:10, id:100}]
  */
-function listToTree(source) {
-    if (source.length === 0) {
-        return []
-    } else {
-        let cloneData = JSON.parse(JSON.stringify(source));       // 对源数据深度克隆
+function listToTree(source, rawOptions = {}) {
+    const {
+        idFieldName = "id", // id FieId 的名称 默认为 'id'
+        parentIDFieldName = "parentID", // parentID FieId 的名称 默认为 'parentID'
+        childrenFieldName = "children", // children FieId 的名称 默认为 'children'
+        ignoreOrphans = false // 是否忽略孤儿节点
+    } = rawOptions;
+    //只处理数组结构
+    if (source && Array.isArray(source)) {
+        let cloneData = JSON.parse(JSON.stringify(source));  // 对源数据深度克隆
         return cloneData.filter(father => {                      // 循环所有项，并添加 children 属性
-            let branchArray = cloneData.filter(child => father.id === child.parentID);  // 返回每一项的子级数组
-            if (branchArray.length > 0) {
-                father.children = branchArray
+            const fatherID = father[idFieldName]; //父 id
+            const grandpaID = father[parentIDFieldName]; //爷爷 id
+            //判断是否为孤儿 , 如果查询不到他的父级节点 那么他就是孤儿
+            let isOrphan = false;
+            if (!ignoreOrphans) {
+                isOrphan = cloneData.filter(child => grandpaID === child[idFieldName]).length === 0;  // 返回每一项的子级数组
             }
-            return Number(father.parentID) === 0; // 返回第一层
+            if (fatherID) {
+                let fatherChildren = cloneData.filter(child => fatherID === child[parentIDFieldName]);  // 返回每一项的子级数组
+                if (fatherChildren.length > 0) {
+                    father[childrenFieldName] = fatherChildren;
+                }
+            }
+            const fatherFatherID = father[parentIDFieldName];
+            //最顶级节点为 父节点为空 或 孤儿
+            return fatherFatherID === null || fatherFatherID === undefined || isOrphan;
         });
+    } else {
+        console.warn("listToTree : 数据为空或数据格式有误 (正确情况应为 Array) !!!")
+        return [];
     }
+
 }
 
 export {listToTree}
