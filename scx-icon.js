94d093be742396e25e120766ac456ce2a1244594
scx567888 <scx567888@outlook.com>
2022-02-16 10:46:24 +0800

    1,代码优化

--- scx-icon.js
+++ scx-icon.js
@@ -1,18 +1,3 @@
-import './_scx-icon/index.css'
-import {h} from "vue";
-
-const ScxIcon = {
-    name: 'scx-icon',
-    props: {
-        icon: {
-            type: String,
-            default: '',
-            required: true
-        }
-    },
-    setup(props) {
-        return () => h("svg", {class: 'scx-icon'}, h('use', {href: '#scx-icon_' + props.icon}, []))
-    }
-};
+import ScxIcon from './_scx-icon/index.vue'
 
 export {ScxIcon}
