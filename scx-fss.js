573dbb5923a489b94aa545fc3a41b6816682bf16
scx567888 <scx567888@outlook.com>
2022-06-13 13:05:58 +0800

    1.3.5

--- scx-fss.js
+++ scx-fss.js
@@ -1,5 +1,5 @@
 import SparkMD5 from "spark-md5";
-import {JsonVOError} from "./scx-req.js";
+import {JsonVOError, ScxJsonVoReq} from "./scx-json-vo-req.js";
 import {percentage} from "./vanilla-percentage.js";
 
 class FSSObject {
@@ -24,7 +24,8 @@ class FSSObject {
  * req
  */
 class ScxFSS {
-    scxReq
+    scxReq;
+    scxJsonVoReq;
     maxUploadSize = 10 * 1024 * 1024 * 1024;//最大上传文件 写死 10GB
     chunkSize = 2 * 1024 * 1024;//切片大小 这里写死 2MB
 
@@ -34,6 +35,7 @@ class ScxFSS {
      */
     constructor(scxReq) {
         this.scxReq = scxReq;
+        this.scxJsonVoReq = new ScxJsonVoReq(this.scxReq);
     }
 
     /**
@@ -227,7 +229,7 @@ class ScxFSS {
                     uploadFormData.append('nowChunkIndex', i + '');
 
                     //向后台发送请求
-                    this.scxReq.post(ScxFSS.uploadURL(), uploadFormData).then(data => {
+                    this.scxJsonVoReq.post(ScxFSS.uploadURL(), uploadFormData).then(data => {
                         //这里因为有断点续传的功能所以可以直接设置 i 以便跳过已经上传过的区块
                         if (data.type === 'need-more') {
                             i = data.item;
@@ -242,7 +244,7 @@ class ScxFSS {
                 }
 
                 //这里先检查一下服务器是否已经有相同MD5的文件了 有的话就不传了
-                this.scxReq.post(ScxFSS.checkAnyFileExistsByThisMD5URL(), {
+                this.scxJsonVoReq.post(ScxFSS.checkAnyFileExistsByThisMD5URL(), {
                     fileName,
                     fileSize,
                     fileMD5: md5
@@ -271,7 +273,7 @@ class ScxFSS {
      */
     info(fssObjectID) {
         return new Promise((resolve, reject) => {
-            this.scxReq.post(ScxFSS.infoURL(), {fssObjectID}).then(data => {
+            this.scxJsonVoReq.post(ScxFSS.infoURL(), {fssObjectID}).then(data => {
                 resolve(data ? new FSSObject(data) : null);
             }).catch(e => {
                 reject(e)
@@ -286,7 +288,7 @@ class ScxFSS {
      */
     listInfo(fssObjectIDs) {
         return new Promise((resolve, reject) => {
-            this.scxReq.post(ScxFSS.listInfoURL(), {fssObjectIDs}).then(data => {
+            this.scxJsonVoReq.post(ScxFSS.listInfoURL(), {fssObjectIDs}).then(data => {
                 const fssObjectList = data.map(i => new FSSObject(i));
                 resolve(fssObjectList);
             }).catch(e => {
--- scx-json-vo-req.js
+++ scx-json-vo-req.js
@@ -32,7 +32,6 @@ class ScxJsonVoReq {
 
     scxReq
 
-
     constructor(scxReq) {
         this.scxReq = scxReq;
     }
